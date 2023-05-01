d32ab46258ffd0a87c0d6a6642b0861074554700
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-04-30 21:31:24 -0300

    Atualizando Application

--- app.js
+++ app.js
@@ -1,42 +1,66 @@
-
 const formAddTodo = document.querySelector('.form-add-todo')
 const todosContainer = document.querySelector('.todos-container')
 const formSearch = document.querySelector('.form-search')
 
+const addTodo = valueInput => {
+    if (valueInput.length) {
+        todosContainer.innerHTML += `
+            <li class="list-group-item d-flex justify-content-between align-items-center" data-todo="${valueInput}">
+                <span>${valueInput}</span>
+                <i class="far fa-trash-alt " data-trash="${valueInput}"></i>
+            </li>`
+            
+        event.target.reset()
+    }
+}
+
 formAddTodo.addEventListener('submit', event => {
     event.preventDefault()
     const valueInput =  event.target.add.value.trim()
-    if (valueInput.length) {
-        todosContainer.innerHTML += `
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-                <span>${valueInput}</span>
-                <i class="far fa-trash-alt delete"></i>
-            </li>`
-
-    }
-    event.target.reset()
+    addTodo(valueInput)
 })
+
+const removeTodo = clickedElement => {
+    const todoElement = clickedElement.dataset.trash 
+    const referenciaDaLi = document.querySelector(`[data-todo="${todoElement}"]`)
+
+    if (todoElement) {
+        referenciaDaLi.remove()
+    }
+}
 
 todosContainer.addEventListener('click', event => {
     const clickedElement =  event.target
-
-    if(Array.from(clickedElement.classList).includes('delete')) {
-        clickedElement.parentElement.remove()
-    }
+    removeTodo(clickedElement)
 })
+
+const fintrandoTodos = (todos, filtrando, returnMethedValue) => todos
+    .filter(todo => {
+        const methedTodos = todo.textContent.toLowerCase().includes(filtrando)
+        return returnMethedValue ? methedTodos : !methedTodos
+    })
+
+const alterandoClassList = (todos, classAdd, classRemove) => {
+    todos.forEach(todo => {
+        todo.classList.add(classAdd)
+        todo.classList.remove(classRemove)
+    })
+}
+
+const hiDeTodos = (todos, filtrando) => {
+    const todosAdd = fintrandoTodos(todos, filtrando, false)
+    alterandoClassList(todosAdd, 'hedden', 'd-flex')
+}
+
+const showTodos = (todos, filtrando) => {
+   const todosRemove = fintrandoTodos(todos, filtrando, true)
+   alterandoClassList(todosRemove, 'd-flex', 'hedden')
+}
 
 formSearch.addEventListener('input', event => {
     const filtrando = event.target.value.toLowerCase()
-    Array.from(todosContainer.children)
-    .filter(todo => !todo.textContent.toLowerCase().includes(filtrando))
-    .forEach(todo => {
-        todo.classList.add('hedden')
-        todo.classList.remove('d-flex')
-    })
-    Array.from(todosContainer.children)
-    .filter(todo => todo.textContent.toLowerCase().includes(filtrando))
-    .forEach(todo => {
-        todo.classList.add('d-flex')
-        todo.classList.remove('hedden')
-    })
+    const todos = Array.from(todosContainer.children) 
+
+    hiDeTodos(todos, filtrando)
+    showTodos(todos, filtrando)
 })
