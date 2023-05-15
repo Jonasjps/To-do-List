97c51b01396323071d499636b0afbd3012b5a29b
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-05-14 21:53:09 -0300

    Salvando

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const formAddTodo  = document.querySelector('.form-add-todo') 
+const todosContainer = document.querySelector('.todos-container')
 
 formAddTodo.addEventListener('submit', event => {
     event.preventDefault()
-    console.log(event.target.add.value.trim())
+    // console.log(event.target.add.value.trim())
+    
 })
