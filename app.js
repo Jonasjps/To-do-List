96504800845c84a67ab97c511247e82e6fb02103
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-05-14 21:57:58 -0300

    Salvando

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const todosContainer = document.querySelector('.todos-container')
 
 formAddTodo.addEventListener('submit', event => {
     event.preventDefault()
-    // console.log(event.target.add.value.trim())
-    
+    const valueDoInput = event.target.add.value.trim()
+    todosContainer.innerHTML += `
+        <li class="list-group-item d-flex justify-content-between align-items-center" data-todo="Assistir Breaking Bad">
+            <span>${valueDoInput}</span>
+            <i class="far fa-trash-alt delete" data-trash="Assistir Breaking Bad"></i>
+       </li>
+    `
 })
