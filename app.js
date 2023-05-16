db257fa4e8ea936ea877d1514adb98881ad1d57b
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-05-15 22:05:06 -0300

    Salvando

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ formAddTodo.addEventListener('submit', event => {
     event.preventDefault()
 
     const valueDoInput = event.target.add.value.trim()
-    
+
     if (valueDoInput.length) {
         todosContainer.innerHTML += `
             <li class="list-group-item d-flex justify-content-between align-items-center" data-todo="Assistir Breaking Bad">
@@ -15,4 +15,12 @@ formAddTodo.addEventListener('submit', event => {
         `
     }
     event.target.reset()
+})
+
+todosContainer.addEventListener('click', event => {
+    const valueDoInput = event.target
+    if (Array.from(valueDoInput.classList).includes('delete')) {
+        valueDoInput.parentElement.remove()
+    }
+    // console.log(obtendoResultado)
 })
