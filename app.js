e8e9628864da93b5184888f39264579f398afa7b
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-05-21 13:07:38 -0300

    Criasndo Application Js

---
+++ app.js
@@ -0,0 +1,16 @@
+const formAddTodo = document.querySelector('.form-add-todo')
+const todosContainer = document.querySelector('.todos-container')
+
+formAddTodo.addEventListener('submit', event => {
+    event.preventDefault()
+    const valueDoInput = event.target.add.value.trim()
+  if(valueDoInput.length) {
+      todosContainer.innerHTML += ` 
+          <li class="list-group-item d-flex justify-content-between align-items-center" >
+              <span>${valueDoInput}</span>
+              <i class="far fa-trash-alt delete"></i>
+          </li>`
+     }
+
+  event.target.reset()
+})
