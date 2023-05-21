116d656658b109cb9d86221221607e84aec555fc
Jonas Pessoa <jonaspsjw@hotmail.com>
2023-05-21 15:48:27 -0300

    Criação de Application Js finalizada

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const formAddTodo = document.querySelector('.form-add-todo')
 const todosContainer = document.querySelector('.todos-container')
+const formSearch = document.querySelector('.form-search')
 
 formAddTodo.addEventListener('submit', event => {
     event.preventDefault()
@@ -11,7 +12,6 @@ formAddTodo.addEventListener('submit', event => {
               <i class="far fa-trash-alt delete"></i>
           </li>`
      }
-
   event.target.reset()
 })
 
@@ -22,4 +22,21 @@ todosContainer.addEventListener('click', event => {
    if (removElement) {
     clickedElement.parentElement.remove()
    }
+})
+
+formSearch.addEventListener('input', event => {
+    const filtrandoInputValue = event.target.value
+    Array.from(todosContainer.children)
+        .filter(todo => !todo.textContent.toLowerCase().includes(filtrandoInputValue))
+        .forEach(todo => {
+            todo.classList.remove('d-flex')
+            todo.classList.add('hidden')
+        })
+
+        Array.from(todosContainer.children)
+        .filter(todo => todo.textContent.toLowerCase().includes(filtrandoInputValue))
+        .forEach(todo => {
+            todo.classList.remove('hidden')
+            todo.classList.add('d-flex')
+        })
 })
